e6aaaa3ccb10d50577d0a25bf8ae040227ee595c
Julio Cesar <julio15.zn@gmail.com>
2021-04-24 00:41:19 -0300

    feat: jwt implementation

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -16,48 +16,69 @@ using Microsoft.OpenApi.Models;
 
 namespace application
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            
-            // Dependency Injection Users
-            ConfigureService.ConfigureDependenciesService(services);
-            ConfigureRepository.ConfigureDependenciesRepository(services);
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Curso de API com AspNetCore", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "application v1"));
-            }
-            
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      // Dependency Injection Users
+      ConfigureService.ConfigureDependenciesService(services);
+      ConfigureRepository.ConfigureDependenciesRepository(services);
+
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "Curso de API com AspNetCore",
+          Version = "v1",
+          Description = "Arquitetura DDD",
+          TermsOfService = new Uri("https://portfoliojulio.netlify.com/"),
+          Contact = new OpenApiContact
+          {
+            Name = "Julio Cesar Martins da Silva",
+            Email = "julio15.zn@gmail.com",
+            Url = new Uri("https://portfoliojulio.netlify.com/")
+          },
+          License = new OpenApiLicense
+          {
+            Name = "Termo de Licença de Uso",
+            Url = new Uri("https://portfoliojulio.netlify.com/")
+          }
+        });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.1");
+          c.RoutePrefix = string.Empty;
+        });
+      }
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -6,14 +6,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.CrossCutting.DependencyInjection
 {
-    public class ConfigureRepository
+  public class ConfigureRepository
+  {
+    public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
     {
-        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
-        {
-            serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
-            serviceCollection.AddDbContext<MyContext>(
-                options => options.UseMySql("Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=root;")
-            );
-        }
+      serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+      serviceCollection.AddDbContext<MyContext>(
+          options => options.UseMySql("Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=root;")
+      );
     }
+  }
 }
